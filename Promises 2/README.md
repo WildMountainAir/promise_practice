ace07ce67e35bbcc3478b27fa8d425728c8ff0ee
Alison Sipos <alison.sipos@icloud.com>
2020-09-23 10:11:13 -0600

    Practice with Promises and Callbacks.

--- Promises 2/README.md
+++ Promises 2/README.md
@@ -1,15 +1,26 @@
-#Promises Notes
+# Promises Notes
 
 A promise in javascript is just like a promise in real life. You can promise to do something there are two possible results.
 1) that promise is completed and therefore resolved
-2) that promise is failed and there fore rejected
+2) that promise is failed and therefore rejected
 
 ## Syntax
 A new Promise takes in an anonymous function that takes in a resolve and a reject. 
 Define what the actual promise is 
-If the promise completes you would resolve it
-You can pass anything into the resolve function, like a success message.
-Else if the promise fails you reject it
-You can pass anything into the reject function, like a failed message.
+If the promise completes you would resolve it using the resolve method
+- You can pass anything into the resolve function, like a success message.
+Else if the promise fails you reject it using the reject method
+- You can pass anything into the reject function, like a failed message.
 
-Anything in a .then() is going to happen after a resolve
+Anything in a .then() is going to happen after/for a resolve
+.then takes in a method, define that method
+
+To catch errors use the .catch() method, which will catch any errors which are our reject states
+.catch takes in a method, define that method
+
+.then is called when our Promise resolves successfully, and .catch is called when our Promise fails and is rejected.
+
+## Notes
+Promises are useful when you want something done in the background that might take some time, like downloading an image from a different server.
+
+Without promises callbacks can require you to nest and nest and nest causing "callback hell", promises allow you to chain .then methods to avoid
--- Promises 2/promises2.js
+++ Promises 2/promises2.js
@@ -1,10 +1,67 @@
-let p = new Promise((resolve, reject) => {
-    var a = 1 + 1;
-    if (a === 2) {
-        resolve('Success');
-    } else {
-        reject('Failed');
-    }
-})
+// Promise Syntax
+// let p = new Promise((resolve, reject) => { // takes in resolve and reject
+//     let a = 1 + 1; // promise definition
+//     if (a === 2) { // conditions for fail or success
+//         resolve('Success'); // resolve method called
+//     } else {
+//         reject('Failed'); // reject method called
+//     }
+// })
 
-p.then()
+// p.then((message) => { // if successful
+//     console.log('This is in the then ' + message);
+// }).catch((message) => { // if error
+//     console.log('This is in the catch ' + mesage);
+// })
+
+// Promises with Callbacks
+const userLeft = false;
+const userWatchingCatMeme = false;
+// WITH CALLBACKS
+const watchTutorialCallback = (callback, errorCallback) => {
+  if (userLeft) {
+      errorCallback({
+          name: 'User Left',
+          message: ':('
+      });
+  } else if (userWatchingCatMeme) {
+      errorCallback({
+          name: 'User Watching Cat Meme',
+          message: 'WebDevSimplified < Cat'
+      });
+  } else {
+      callback('Thumbs Up and Subscribe');
+  }
+}
+
+watchTutorialCallback((message) => {
+    console.log('Success ' + message);
+}, (error) => {
+    console.log(error.name + ' ' + error.message);
+});
+// WITH PROMISES
+const watchTutorialPromise = () => { // we can remove callback params because we don't need to use them with Promises
+    return new Promise((resolve, reject) => {// return a promise which takes in 2 params resolve and reject
+        if (userLeft) {
+            reject({ // errorCallback is now the reject method
+                name: 'User Left',
+                message: ':('
+            });
+        } else if (userWatchingCatMeme) {
+            reject({// errorCallback is now the reject method
+                name: 'User Watching Cat Meme',
+                message: 'WebDevSimplified < Cat'
+            });
+        } else { // callback is now the resolve method
+            resolve('Thumbs Up and Subscribe');
+        }
+    }); 
+  }
+
+  watchTutorialPromise() // we dont need to take any callback params in
+    .then((message) => { // resolve method
+    console.log('Success ' + message);
+    })
+    .catch((error) => { // reject method
+    console.log(error.name + ' ' + error.message);
+});
